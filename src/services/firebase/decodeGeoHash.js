b5b5246b1d460f3b21d56c25aaed6587a2b83e56
Yab1 <yabilisanu@gmail.com>
2023-06-24 18:57:13 +0300

    Updates

--- src/services/firebase/decodeGeoHash.js
+++
@@ -1,6 +0,0 @@
-import Geohash from "latlon-geohash";
-
-export default function decodeGeoHash(geohash) {
-  const { lat, lon } = Geohash.decode(geohash);
-  return { latitude: lat, longitude: lon };
-}
